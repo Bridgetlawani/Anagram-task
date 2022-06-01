4ea401b11a078a9a717e8c42cdec54423047ebea
Bridget Lawani <105175902+Bridgetlawani@users.noreply.github.com>
2022-06-01 07:21:43 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,77 +1,21 @@
-import random
+# Check if a word is an anagrams
+# Example:
+# find_anagrams("hello") --> False
+# find_anagrams("racecar") --> True
 
 
-print("Welcome")
-is_running = True
+def find_anagrams(word1, word2):
+    # [assignment] Add your code here
+    # find_anagrams["earth", "heart"]
+    word1 = word1.strip()
+    word2 = word2.strip()
 
+    if(sorted(word1) == sorted(word2)):
+        print(True)
+    else:
+        print('false')
 
-
-while is_running:
-    print("Starting Game")
-    choices = ["rock :R","paper:P", "scissors:S"]
-    #player = input("rock, paper, or scissors \n")
-    player = input("R,P or S \n")
-    computer = random.choice(choices)
-
-    try:
-        
-        #print(computer)
-        while player in choices
-        #print(player)    
-        is_running = False
-
-    except:
-        player not in choices
-        print("Invalid Input, Try Again!")
-        continue
-
-    if player == computer:
-       print("computer: ",computer)
-       print("player: ",player)
-       print("Tie!")
-
-    elif player == "rock":
-        if computer == "paper":
-           print("computer: ",computer)
-           print("player: ",player)
-           print("You lose!")
-
-    elif player == "paper":
-       if computer == "rock":
-          print("computer: ",computer)
-          print("player: ",player)
-          print("You win")
-
-    elif player == "scissors":
-       if computer == "paper":
-          print("computer: ",computer)
-          print("player: ",player)
-          print("You win!")
-
-    elif player == "paper":
-        if computer == "scissors":
-          print("computer: ",computer)
-          print("player: ",player)
-          print("You lose!")
-
-    elif player == "scissors":
-        if computer == "rock":
-          print("computer: ",computer)
-          print("player: ",player)
-          print("You lose!")
-
-    elif player == "rock":
-        if computer == "scisoors":
-          print("computer: ",computer)
-          print("player: ",player)
-          print("You win!")
-
-play_again = input("Would you like to play again? [yes/no]: ")
-if play_again == "yes":
-    is_running = True
-
-if play_again == "no":
-    print("Thank you for playing")
-    is_running = False
-
+        # return True
+        #print(find_anagrams('earth', 'heart'))
+find_anagrams('peach', 'cheap')
 
